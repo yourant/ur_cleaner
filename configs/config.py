cd630c785d208d76fb8b58d0f434b4b05e30a468
turpure <p812816627@163.com>
2018-10-18 14:42:42 +0800

    read config

--- configs/config.py
+++ configs/config.py
@@ -4,6 +4,7 @@
 # Author: turpure
 
 import os
+import json
 
 
 class Singleton(object):
@@ -15,11 +16,21 @@ class Singleton(object):
 
 class Config(Singleton):
     def __init__(self, env):
-        path_list = str.split(os.path.split(os.path.realpath(__file__))[0], os.sep) + [env]
-        print(path_list)
+        path_list = str.split(os.path.split(os.path.realpath(__file__))[0], os.sep) + [env, 'config.json']
+        config_path = os.sep.join(path_list)
+        self.config = self._load_config(config_path)
+
+    @staticmethod
+    def _load_config(path):
+        with open(path, encoding='utf-8') as con:
+            return json.load(con)
+
+    def get_config(self, key):
+        return self.config[key]
 
 
 if __name__ == '__main__':
     config = Config('dev')
+    print(config.get_config('mssql'))
 
 
