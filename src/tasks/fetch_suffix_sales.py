4ff26b524d3db6501d2cf33bdbbf285616c42f1e
turpure <p812816627@163.com>
2018-11-01 10:15:21 +0800

    new feture

--- src/tasks/fetch_suffix_sales.py
+++ src/tasks/fetch_suffix_sales.py
@@ -19,14 +19,24 @@ class Fetcher(BaseService):
         self.cur.execute(sql, (date_flag, begin_date, end_date))
         ret = self.cur.fetchall()
         for row in ret:
-            yield (row['suffix'], row['orderTime'], row['amt'], row['dateFlag'])
+            yield (row['suffix'], row['orderTime'], float(row['amt']), row['dateFlag'])
 
-    def push(self, row):
+    def push(self, rows):
         sql = 'insert into cache_suffixSales(suffix,orderTime,amt,dateFlag) values(%s,%s,%s, %s)'
-        self.warehouse_cur.executemany(sql,)
+        self.warehouse_cur.executemany(sql, list(rows))
+        self.warehouse_con.commit()
+
+    def work(self):
+        try:
+            rows = self.fetch(1, '2018-10-01', '2018-10-10')
+            self.push(rows)
+            self.logger.info('success to fetch suffix sales')
+        except Exception as why:
+            self.logger.error('fail to fetch suffix sales cause of {}'.format(why))
+        finally:
+            self.close()
 
 
 if __name__ == '__main__':
     worker = Fetcher()
-    for row in worker.fetch(0, '2018-10-01', '2018-10-10'):
-        print(row)
+    worker.work()
