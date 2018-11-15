a276e9d05eb8df715569d4bab3ac8a8bced15610
turpure <p812816627@163.com>
2018-11-15 10:43:22 +0800

    sync data in time

--- src/tasks/fetch_suffix_sales.py
+++ src/tasks/fetch_suffix_sales.py
@@ -20,10 +20,16 @@ class Fetcher(BaseService):
         self.cur.execute(sql, (date_flag, begin_date, end_date))
         ret = self.cur.fetchall()
         for row in ret:
-            yield (row['suffix'], row['orderTime'], float(row['amt']) if row['amt'] else 0, row['dateFlag'])
+            yield (row['suffix'],
+                   row['orderTime'],
+                   float(row['amt']) if row['amt'] else 0,
+                   row['dateFlag'],
+                   )
 
     def push(self, rows):
-        sql = 'insert into cache_suffixSales(suffix,orderTime,amt,dateFlag) values(%s,%s,%s, %s)'
+        sql = ('insert into cache_suffixSales(suffix,orderTime,amt,dateFlag) values(%s,%s,%s, %s)'
+                ' ON DUPLICATE KEY UPDATE amt=values(amt)'
+               )
         self.warehouse_cur.executemany(sql, list(rows))
         self.warehouse_con.commit()
 
@@ -32,9 +38,10 @@ class Fetcher(BaseService):
 
     def work(self):
         try:
-            yesterday = str(datetime.datetime.today() - datetime.timedelta(days=1))[:10]
+            today = str(datetime.datetime.today())
+            four_days_ago = str(datetime.datetime.today() - datetime.timedelta(days=4))[:10]
             for date_flag in [0, 1]:
-                rows = self.fetch(date_flag, yesterday, yesterday)
+                rows = self.fetch(date_flag, four_days_ago, today)
                 self.push(rows)
                 self.logger.info('success to fetch suffix sales')
         except Exception as why:
