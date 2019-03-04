217a81686b64d52a1f9cc427864fbe27abd552e7
turpure <p812816627@163.com>
2019-03-04 13:02:03 +0800

    counting cache

--- src/tasks/fetch_suffix_profit.py
+++ src/tasks/fetch_suffix_profit.py
@@ -21,7 +21,7 @@ class ProfitFetcher(BaseService):
         ret = self.cur.fetchall()
         for row in ret:
             yield (
-                row['suffix'], row['SaleMoney'], row['salemoneyzn'], row['eBayFeeebay'], row['eBayFeeznebay'],
+                row['suffix'], row['plat'], row['SaleMoney'], row['salemoneyzn'], row['eBayFeeebay'], row['eBayFeeznebay'],
                 row['ppFee'], row['ppFeezn'], row['CostMoney'], row['ExpressFare'], row['InpackageMoney'],
                 row['StoreName'], row['refund'], row['diefeeZn'], row['suffixFee'], row['opeFee'],
                 row['grossProfit'], row['orderDay'], row['dateFlag']
@@ -29,7 +29,7 @@ class ProfitFetcher(BaseService):
 
     def push(self, rows):
         sql = ['insert into cache_suffixProfit(',
-               'suffix,saleMoney,saleMoneyZn,ebayFeeEbay,',
+               'suffix,plat,saleMoney,saleMoneyZn,ebayFeeEbay,',
                'ebayFeeZnEbay,ppFee,ppFeeZn,costMoney,',
                'expressFare,inPackageMoney,storeName,',
                'refund,dieFeeZn,insertionFee,saleOpeFeeZn,',
@@ -38,8 +38,8 @@ class ProfitFetcher(BaseService):
                '%s,%s,%s,%s,%s,',
                '%s,%s,%s,%s,%s,',
                '%s,%s,%s,%s,%s,',
-               '%s,%s,%s',  
-               ') ON DUPLICATE KEY UPDATE saleMoney=values(saleMoney),'
+               '%s,%s,%s,%s',
+               ') ON DUPLICATE KEY UPDATE saleMoney=values(saleMoney),plat=values(plat),'
                'saleMoneyZn=values(saleMoneyZn),ebayFeeEbay=values(ebayFeeEbay),'
                'ebayFeeZnEbay=values(ebayFeeZnEbay),ppFee=values(ppFee),'
                'ppFeeZn=values(ppFeeZn),costMoney=values(costMoney),'
