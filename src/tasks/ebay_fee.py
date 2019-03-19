f7b243a08619af35caa3a7b255449eef1f60ed93
turpure <p812816627@163.com>
2019-03-19 14:41:29 +0800

    rewrite ebay-fee

--- src/tasks/ebay_fee.py
+++ src/tasks/ebay_fee.py
@@ -8,6 +8,7 @@ import datetime
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from tenacity import retry, stop_after_attempt
 from ebaysdk.trading import Connection as Trading
+from ebaysdk import exception
 from src.services.base_service import BaseService
 from configs.config import Config
 import pymssql
@@ -21,7 +22,7 @@ class EbayFee(BaseService):
         super().__init__()
         self.config = Config().get_config('ebay.yaml')
         if not self._get_batch_id():
-            self.batch_id = str(datetime.datetime.now() - datetime.timedelta(days=1))[:10]
+            self.batch_id = str(datetime.datetime.now() - datetime.timedelta(days=2))[:10]
         else:
             self.batch_id = str(datetime.datetime.strptime(self._get_batch_id(), '%Y-%m-%d')
                                 + datetime.timedelta(days=1))[:10]
@@ -41,77 +42,101 @@ class EbayFee(BaseService):
 
     def get_ebay_token(self):
         sql = ("SELECT notename,max(ebaytoken) AS ebaytoken "
-               "FROM S_PalSyncInfo  GROUP BY notename")
-               # " having notename='eBay-A2-littlemay93'")
+               "FROM S_PalSyncInfo  GROUP BY notename"
+               " having notename='eBay-A6-vitalityang1'")
         self.cur.execute(sql)
         ret = self.cur.fetchall()
         for row in ret:
             yield row
 
-    @retry(stop=stop_after_attempt(3))
     def get_ebay_fee(self, ebay_token):
+        par = self.get_request_params(ebay_token)
+        for ret in self._get_ebay_fee(par):
+            for fee in self._parse_response(ret, ebay_token):
+                yield fee
+
+    def get_request_params(self, ebay_token):
         """
         get the ebay fee of yesterday in local time
         """
 
-        # begin_date = str(datetime.datetime.now() - datetime.timedelta(days=1))[:10]
-        # begin_date = self.batch_id
-        begin_date = '2019-03-02'
-        # end_date = str(datetime.datetime.now())[:10]
-        end_date = '2019-03-08'
+        begin_date = self.batch_id
+        end_date = str(datetime.datetime.now())[:10]
         begin_date += "T00:00:00.000Z"
         end_date += "T01:00:00.000Z"  # utc time
-        try:
-            api = Trading(siteid=0, config_file=self.config, timeout=40)
-            par = {
-                "RequesterCredentials": {"eBayAuthToken": ebay_token['ebaytoken']},
-                # "AccountEntrySortType": "AccountEntryFeeTypeAscending",
-                "AccountEntrySortType": "AccountEntryCreatedTimeDescending",
-                "AccountHistorySelection": "BetweenSpecifiedDates",
-                "Currency": 'USD',
-                "BeginDate": begin_date,
-                "EndDate": end_date,
-                "Pagination": {"EntriesPerPage": 2000, "PageNumber": 1}
-            }
-            currency = ['USD', 'GBP']
-            for cur in currency:
-                par['Currency'] = cur
-                try:
-                    response = api.execute('GetAccount', par)
-                except Exception:
-                    par.pop("Currency")
-                    response = api.execute('GetAccount', par)
-                total_pages = int(response.reply.PaginationResult.TotalNumberOfPages)
-                for num in range(0, total_pages):
-                    par['Pagination']['PageNumber'] = num + 1
-                    for i in range(1, 4):
-                        try:
-                            res = api.execute('GetAccount', par)
-                            ret = res.reply.AccountEntries.AccountEntry
-                            for row in ret:
-                                fee_type = row.AccountDetailsEntryType
+        par = {
+            "RequesterCredentials": {"eBayAuthToken": ebay_token['ebaytoken']},
+            "AccountEntrySortType": "AccountEntryCreatedTimeDescending",
+            "AccountHistorySelection": "BetweenSpecifiedDates",
+            "BeginDate": begin_date,
+            "EndDate": end_date,
+            "Pagination": {"EntriesPerPage": 2000, "PageNumber": 1}
+        }
 
-                                if fee_type not in ('FeeFinalValue', 'FeeFinalValueShipping',
-                                                    'PayPalOTPSucc',
-                                                    'PaymentCC', 'CreditFinalValue', 'CreditFinalValueShipping', 'Unknown'
-                                                    ):
-                                    fee = dict()
-                                    fee['feeType'] = fee_type
-                                    fee['Date'] = str(row.Date)
-                                    fee['value'] = row.NetDetailAmount.value
-                                    fee['currency'] = row.NetDetailAmount._currencyID
-                                    fee['accountName'] = ebay_token['notename']
-                                    fee['ItemID'] = row.ItemID
-                                    if float(row.NetDetailAmount.value) >= 10 or float(row.NetDetailAmount.value) <= -10:
-                                        self.logger.warning('%s:%s' % (fee_type, float(row.NetDetailAmount.value)))
-                                    if float(row.NetDetailAmount.value) != 0:
-                                        yield fee
-                            break
-                        except Exception as e:
-                            self.logger.error("trying %s times but %s" % (i, e))
-        except Exception as e:
-            self.logger.error('%s while getting ebay fee' % e)
-            raise Exception(e)
+        return par
+
+    def _get_ebay_fee(self, par):
+        api = Trading(siteid=0, config_file=self.config, timeout=40)
+        currency = ['GBP', 'USD']
+        for cur in currency:
+            try:
+                par['Currency'] = cur
+                response = api.execute('GetAccount', par)
+                try:
+                    total_pages = int(response.reply.PaginationResult.TotalNumberOfPages)
+                    if total_pages == 1:
+                        ret = response.reply.AccountEntries.AccountEntry
+                        yield ret
+                    if total_pages > 1:
+                        ret = response.reply.AccountEntries.AccountEntry
+                        yield ret
+                        for page in range(1, total_pages):
+                            par['Pagination']['PageNumber'] = page
+                            response = api.execute('GetAccount', par)
+                            ret = response.reply.AccountEntries.AccountEntry
+                            yield ret
+                except Exception as why:
+                    self.logger.error('error while getting accountEntry cause of {}'.format(why))
+
+            except exception.ConnectionError as why:
+                self.logger.warning(why)
+                par.pop('Currency')
+                response = api.execute('GetAccount', par)
+                total_pages = int(response.reply.PaginationResult.TotalNumberOfPages)
+                if total_pages == 1:
+                    ret = response.reply.AccountEntries.AccountEntry
+                    yield ret
+                if total_pages > 1:
+                    ret = response.reply.AccountEntries.AccountEntry
+                    yield ret
+                    for page in range(1, total_pages):
+                        par['Pagination']['PageNumber'] = page
+                        response = api.execute('GetAccount', par)
+                        ret = response.reply.AccountEntries.AccountEntry
+                        yield ret
+                break
+                # to-do read time out exception
+            except Exception as why:
+                self.logger.error(why)
+
+    def _parse_response(self, ret, ebay_token):
+        for row in ret:
+            fee_type = row.AccountDetailsEntryType
+            if fee_type not in ('FeeFinalValue', 'FeeFinalValueShipping',
+                                'PayPalOTPSucc',
+                                'PaymentCC', 'CreditFinalValue', 'CreditFinalValueShipping', 'Unknown'
+                                ):
+                fee = dict()
+                fee['feeType'] = fee_type
+                fee['Date'] = str(row.Date)
+                fee['value'] = row.NetDetailAmount.value
+                fee['currency'] = row.NetDetailAmount._currencyID
+                fee['accountName'] = ebay_token['notename']
+                fee['ItemID'] = row.ItemID
+                if float(row.NetDetailAmount.value) >= 10 or float(row.NetDetailAmount.value) <= -10:
+                    self.logger.warning('%s:%s' % (fee_type, float(row.NetDetailAmount.value)))
+                if float(row.NetDetailAmount.value) != 0:
+                    yield fee
 
     def save_data(self, row):
         sql = 'insert into y_fee(notename,fee_type,total,currency_code,fee_time,batchId)' \
