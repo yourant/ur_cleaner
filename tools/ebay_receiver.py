5d4b6c70d44ef3670d00a594f61aec2277f3bdc8
turpure <p812816627@163.com>
2019-04-09 11:40:32 +0800

    set email

--- tools/ebay_receiver.py
+++ tools/ebay_receiver.py
@@ -14,11 +14,11 @@ class EbayHandler(BaseService):
     def get_trades(self):
         sql = ("select pt.nid,ptd.l_number,'p_trade' as tableName from p_trade(nolock) as pt "
                "LEFT JOIN p_tradedt(nolock) as ptd on pt.nid= ptd.tradeNid "
-               "where addressowner='ebay' and isnull(receiverbusiness,'') = '' "
+               "where addressowner='ebay' and isnull(receiveremail,'') = '' "
                "and convert(varchar(7),closingdate,121)='2019-03' union "
                "select pt.nid, ptd.l_number,'P_trade_his' as tableName from p_trade_his(nolock) as pt "
                "LEFT JOIN p_tradedt_his(nolock) as ptd on pt.nid= ptd.tradeNid "
-               "where addressowner='ebay' and isnull(receiverbusiness,'') = '' "
+               "where addressowner='ebay' and isnull(receiveremail,'') = '' "
                "and convert(varchar(7),closingdate,121)='2019-03'")
         self.cur.execute(sql)
         ret = self.cur.fetchall()
@@ -29,11 +29,11 @@ class EbayHandler(BaseService):
         sql = ("select top 1  nid ,l_number, payPal from "
                "(select pt.nid,ptd.l_number,receiverBusiness as payPal "
                "from p_trade(nolock) as pt LEFT JOIN p_tradedt(nolock) as ptd "
-               "on pt.nid= ptd.tradeNid where addressowner='ebay' and isnull(receiverbusiness,'') != '' "
+               "on pt.nid= ptd.tradeNid where addressowner='ebay' and isnull(receiveremail,'') != '' "
                "and l_number=%s UNION "
                "select pt.nid,ptd.l_number,receiverBusiness as payPal "
                "from p_trade_his(nolock) as pt LEFT JOIN p_tradedt_his(nolock) as ptd "
-               "on pt.nid= ptd.tradeNid where addressowner='ebay' and isnull(receiverbusiness,'') != '' "
+               "on pt.nid= ptd.tradeNid where addressowner='ebay' and isnull(receiveremail,'') != '' "
                "and l_number=%s) tap")
         try:
             self.cur.execute(sql, (row['l_number'], row['l_number']))
@@ -44,7 +44,7 @@ class EbayHandler(BaseService):
             self.logger.error('{} fails to get payPal cause of {}'.format(row['nid'], why))
 
     def set_paypal(self, row):
-        sql = 'update {} set receiverbusiness=%s where nid=%s'
+        sql = 'update {} set receiveremail=%s where nid=%s'
         try:
             self.cur.execute(sql.format(row['tableName']), (row['payPal'], row['nid']))
             self.con.commit()
