8a34589fb3b834cc5bbf1c041fb0e553d38f8638
turpure <p812816627@163.com>
2019-04-30 10:47:49 +0800

    fix bug: get trades before picking

--- src/tasks/do_task_pick.py
+++ src/tasks/do_task_pick.py
@@ -16,25 +16,45 @@ class Picker(BaseService):
             for row in ret:
                 yield row
         except Exception as why:
-            self.logger.error('fail to get picking task')
+            self.logger.error('fail to get picking task cause of {}'.format(why))
             yield
 
-    def do_task(self, row):
-        task_sql = "update p_trade set packingMen=%s where batchNum=%s"
-        status_sql = 'update task_pick set updatedTime=now(),isDone=1 where batchNumber=%s'
+    def get_trades(self, row):
+        trades_to_update = ("select tradeNid, pickupNo as batchNumber "
+                            "from P_TradePickup(nolock)  where 1=1  and PickupNo like '%{}%' ")
         try:
-            self.cur.execute(task_sql, (row['picker'], row['batchNumber']))
-            self.warehouse_cur.execute(status_sql, (row['batchNumber']))
-            self.logger.info('{} picked {}'.format(row['picker'], row['batchNumber']))
-            self.warehouse_con.commit()
+            self.cur.execute(trades_to_update.format(row['batchNumber']))
+            ret = self.cur.fetchall()
+            for out in ret:
+                out['picker'] = row['picker']
+                yield out
+
+        except Exception as why:
+            self.logger.error('failed to get trades cause of {}'.format(why))
+
+    def do_task(self, row):
+        task_sql = "update p_trade set packingMen=%s where nid=%s"
+        try:
+            self.cur.execute(task_sql, (row['picker'], row['tradeNid']))
             self.con.commit()
         except Exception as why:
-            self.logger.error('{} failed to pick {} cause of {}'.format(row['picker'], row['batchNumber'], why))
+            self.logger.error('{} failed to pick {} cause of {}'.format(row['tradeNid'], row['batchNumber'], why))
+
+    def after_task(self, row):
+        status_sql = 'update task_pick set updatedTime=now(),isDone=1 where batchNumber=%s'
+        try:
+            self.warehouse_cur.execute(status_sql, (row['batchNumber']))
+            self.warehouse_con.commit()
+            self.logger.info('{} picked {}'.format(row['picker'], row['batchNumber']))
+        except Exception as why:
+            self.logger.error('failed to finish task {} cause of {}'.format(row['batchNumber'], why))
 
     def work(self):
         try:
             for row in self.get_tasks():
-                self.do_task(row)
+                for trades in self.get_trades(row):
+                    self.do_task(trades)
+                self.after_task(row)
         except Exception as why:
             self.logger.error('fail to do task cause of {}'.format(why))
         finally:
