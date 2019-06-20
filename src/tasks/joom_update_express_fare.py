6b031bf69202c9e0d9927a49e95fabdf056e8cf0
turpure <p812816627@163.com>
2019-06-20 20:12:22 +0800

    new task:update jooom express fare

---
+++ src/tasks/joom_update_express_fare.py
@@ -0,0 +1,36 @@
+#! usr/bin/env/python3
+# coding:utf-8
+# @Time: 2019-06-20 20:02
+# Author: turpure
+
+
+from src.services.base_service import BaseService
+import datetime
+
+class Updater(BaseService):
+
+    def get_data(self, begin_date):
+        sql = 'oa_p_joomNullExpressFare %s'
+        self.cur.execute(sql, (begin_date,))
+        self.con.commit()
+
+    def update_date(self):
+        sql = 'oa_p_updateJoomNullExpressFare'
+        self.cur.execute(sql)
+        self.con.commit()
+
+    def work(self):
+        try:
+            begin_date = str(datetime.datetime.today() - datetime.timedelta(days=30))[:10]
+            self.get_data(begin_date)
+            self.update_date()
+            self.logger.info('success to update joom express-fare ')
+        except Exception as why:
+            self.logger.error('failed to update joom express-fare cause of {}'.format(why))
+        finally:
+            self.close()
+
+
+if __name__ == "__main__":
+    worker = Updater()
+    worker.work()
