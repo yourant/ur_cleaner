d90195a5f02a902e045c7a4a8f2b0287668b871e
turpure <p812816627@163.com>
2019-08-16 10:48:47 +0800

    get bearer token

--- src/services/mitm_addons.py
+++ src/services/mitm_addons.py
@@ -15,26 +15,44 @@ class Counter:
     def request(self, flow: mitmproxy.http.HTTPFlow):
         headers = flow.request.headers
         url = flow.request.url
-        for row in headers.items(True):
-            if row[0] == 'x-api-token' and url.startswith('https://www.joom.com/tokens/init'):
+        if url.startswith('https://www.joom.com/tokens/init'):
+            try:
+                headers = self.pares_headers(headers)
+                row = dict()
+                print(headers)
+                row['api_token'] = headers['x-api-token']
+                row['access_token'] = 'Bearer ' + self.parse_cookies(headers['cookie'])['accesstoken']
                 self.save(row)
-                break
+            except Exception as why:
+                print(why)
 
-    def save(self, row):
+        # for row in headers.items(True):
+        #     if row[0] == 'x-api-token' and url.startswith('https://www.joom.com/tokens/init'):
+        #         self.save(row)
+        #         break
+
+    @staticmethod
+    def parse_cookies(cookies):
+        return {item.split('=')[0]: item.split('=')[1] for item in cookies.split('; ')}
+
+    @staticmethod
+    def pares_headers(headers):
+        headers = headers.items(True)
+        return {ele[0]: ele[1] for ele in headers}
+
+    @staticmethod
+    def save(row):
         con = DataBase('mysql')
         try:
             cur = con.connection.cursor()
-            sql = 'update urTools.sys_joom_token set token=%s,updateTime=now() where id=1'
-            cur.execute(sql, row[1])
+            sql = 'update urTools.sys_joom_token set token=%s, bearerToken=%s,updateTime=now() where id=1'
+            cur.execute(sql, (row['api_token'], row['access_token']))
             con.connection.commit()
             print('saving {}'.format(row))
         except Exception as why:
             print(why)
         finally:
             con.close()
-
-
-
 
 addons = [
     Counter()
