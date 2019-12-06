84b1894f0490c61c7b1a04c2e349e965822c1821
turpure <p812816627@163.com>
2019-12-06 13:27:16 +0800

    get images from ebay

---
+++ sync/ebay_spider/__init__.py
@@ -0,0 +1,4 @@
+#! usr/bin/env/python3
+# coding:utf-8
+# @Time: 2019-12-06 10:09
+# Author: turpure
---
+++ sync/ebay_spider/get_images.py
@@ -0,0 +1,81 @@
+#! usr/bin/env/python3
+# coding:utf-8
+# @Time: 2019-12-06 10:11
+# Author: turpure
+
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+import datetime
+
+
+class Worker(object):
+
+    def __init__(self):
+        self.mongo = MongoClient('192.168.0.150', 27017)
+        self.mongodb = self.mongo['product_engine']
+        self.col = self.mongodb['ebay_recommended_product']
+
+    def get_url(self):
+        today = str(datetime.datetime.now())[:10]
+        ret = self.col.find({'recommendDate': {'$regex': today}})
+        for row in ret:
+            ele = dict()
+            ele['itemId'] = row['itemId']
+            url = 'https://www.ebay.com/itm/' + row['itemId']
+            ele['url'] = url
+            yield ele
+
+    def save_one(self, row):
+        try:
+            self.col.find_one_and_update({'itemId': row['itemId']}, {'$set': {'images': row['images']}}, upsert=True)
+        except Exception as why:
+            print(f'fail to save {row["itemId"]} because of {why} ')
+
+    @staticmethod
+    def get_image(url):
+        for i in range(2):
+            try:
+                res = requests.get(url['url'])
+                html = BeautifulSoup(res.text, features='lxml')
+                image_div = html.find('div', {'id': 'vi_main_img_fs'})
+                image_tables = image_div.find_all('table', {'class': 'img'})
+                images = []
+                out = dict()
+                for tb in image_tables:
+                    img = tb.find('img')
+                    src = img.attrs['src']
+                    ret = src.replace('l64', 'l500')
+                    images.append(ret)
+
+                out['images'] = images
+                out['itemId'] = url['itemId']
+                return out
+
+            except Exception as why:
+                print(f'{url["url"]} fail to get image because of {why}')
+        return {'images': [], 'itemId': url['itemId']}
+
+    def trans(self):
+        urls = self.get_url()
+        for ul in urls:
+            ret = self.get_image(ul)
+            self.save_one(ret)
+            print(f'success to get images of {ret["itemId"]}')
+
+    def run(self):
+        try:
+            self.trans()
+        except Exception as why:
+            print(f'fail to get ebay images because of {why}')
+        finally:
+            self.mongo.close()
+
+
+if __name__ == "__main__":
+    worker = Worker()
+    worker.run()
+
+
+
+
