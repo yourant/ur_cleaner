390d756e15fa7a59680c28083565093f04bc0781
tupu <p812816627@163.com>
2020-01-08 10:44:17 +0800

    no cancling

--- src/tasks/fetch_suffix_refund.py
+++ src/tasks/fetch_suffix_refund.py
@@ -61,7 +61,7 @@ class RefundFetcher(BaseService):
                 yesterday = str(datetime.datetime.today() - datetime.timedelta(days=1))[:10]
                 month_first_day = str(datetime.datetime.strptime(yesterday[:8] + '01', '%Y-%m-%d'))[:10]
 
-            # month_first_day = '2019-11-01'
+            # month_first_day = '2019-12-01'
             self.clear(month_first_day, today)
             rows = self.fetch(month_first_day, today)
             self.push(rows)
