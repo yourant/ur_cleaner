569dd9471133b96dce1f3816e2d45df1d1fc89a1
tupu <p812816627@163.com>
2020-03-14 13:28:19 +0800

    add unfinshed status for joom-redo-tasks

--- src/tasks/joom_redo_failed_crlawed_tasks.py
+++ src/tasks/joom_redo_failed_crlawed_tasks.py
@@ -26,7 +26,8 @@ class Worker(BaseService):
         获取采集状态为【待采集】，且2天之内，10分钟之前的任务
         :return:
         """
-        sql = ("select proId from proCenter.oa_dataMine where progress='待采集' and timestampdiff(day,createTime,now())<=30 and  "
+        sql = ("select proId from proCenter.oa_dataMine where progress='待采集'  and detailStatus='未完善'"
+               " and timestampdiff(day,createTime,now())<=30 and  "
                "timestampdiff(MINUTE,createTime,now()) >=5")
         self.warehouse_cur.execute(sql)
         ret = self.warehouse_cur.fetchall()
