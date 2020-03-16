bb2160e5afe562d328a1f05b5fb6e81b7bdb3c49
tupu <p812816627@163.com>
2020-03-16 13:58:32 +0800

    kill chrome_defunct

--- src/tasks/joom_redo_failed_crlawed_tasks.py
+++ src/tasks/joom_redo_failed_crlawed_tasks.py
@@ -27,7 +27,9 @@ class Worker(BaseService):
         plat = sys.platform
         if not plat.startswith('win'):
             kill_chrome_cmd = "ps -ef |grep chrome | awk '{print $2}' | xargs kill -9"
+            kill_chrome_defunct = "ps -ef |grep chrome |grep defunct |awk '{print $2}' |xargs kill -9"
             os.system(kill_chrome_cmd)
+            os.system(kill_chrome_defunct)
             self.logger.info('success to kill chrome ps')
 
     def get_tasks(self):
