46f65e2056d611a4ccf533fd24fb35fd6d70d4d9
tupu <p812816627@163.com>
2020-03-24 10:32:58 +0800

    delete images for tupianku

--- sync/tupianku_image_delete/delete_tupianku_image.py
+++ sync/tupianku_image_delete/delete_tupianku_image.py
@@ -5,62 +5,97 @@
 
 
 import asyncio
+import datetime
+
 import aiohttp
 from sync.tupianku_image_delete.image_server import BaseSpider
-
+from pymongo import MongoClient
+import random
 
 class Worker(BaseSpider):
 
-    def __init__(self):
+    def __init__(self, tupianku_name=2):
         super().__init__()
+        self.tupianku_name = tupianku_name
+        self.mongo = MongoClient('192.168.0.150', 27017)
+        self.mongodb = self.mongo['ur_cleaner']
+        self.col = self.mongodb['delete_tupianku_tasks']
 
-    async def get_goods(self):
+    def _get_goods(self):
         sql = ("SELECT DISTINCT b.goodsCode FROM [dbo].[B_GoodsSKU]  bs LEFT JOIN B_Goods  b ON bs.GoodsID = b.NID" +
-            " WHERE GoodsSKUStatus='停售' AND GoodsID NOT IN (SELECT DISTINCT GoodsID FROM [dbo].[B_GoodsSKU] WHERE GoodsSKUStatus<>'停售')" +
-            " AND  GoodsCode  NOT IN (SELECT DISTINCT GoodsCode FROM [dbo].[TPK_goodsCode_del])" )
+            " WHERE GoodsSKUStatus='停售' AND GoodsID NOT IN (SELECT DISTINCT GoodsID FROM [dbo].[B_GoodsSKU] WHERE GoodsSKUStatus<>'停售')")
         self.cur.execute(sql)
         goods_list = self.cur.fetchall()
-        # print(goods_list)
-        return goods_list
+        for row in goods_list:
+            row['_id'] = row['goodsCode']
+            row['tupianku1'] = 0
+            row['tupianku2'] = 0
+            yield row
 
-    async def deal(self, goodsCode):
-        async with aiohttp.ClientSession() as session:
+    def push_tasks(self):
+        goods_list = self._get_goods()
+        for goods in goods_list:
             try:
-                #登录图片库
-                login_response = await self.log_in(session)
-                login_html = await login_response.text()
-
-                #搜索图片，并获取图片id
-                search_response = await self.search_image(session, goodsCode)
-                search_html = await search_response.text()
-                # print(search_html)
-                image_ids = self.get_image_ids(search_html)
-                # print(image_ids)
-
-                #删除图片
-                if image_ids:
-                    await self.delete_image(session, image_ids)
-
+                self.col.insert_one(goods)
+                self.logger.info(f'success push task of {goods["goodsCode"]}')
             except Exception as why:
-                self.logger.error(f'error while delete image of goodsCode "{goodsCode}" cause of {why}')
+                self.logger.error(f'failed to push task of {goods["goodsCode"]} cause of {why}')
 
+    def pull_tasks(self):
+        tupianku_name = self.tupianku_name
+        try:
+            tasks = self.col.find({f'tupianku{tupianku_name}': 0}).limit(3000)
+            return tasks
+        except Exception as why:
+            self.logger.error(f'failed to pull tasks of tupianku{tupianku_name} cause of {why}')
 
+    async def delete_trans(self, goods_code, sema):
+        try:
+            async with sema:
+                try:
+                    #搜索图片，并获取图片id
+                    image_ids = await self.search_image(goods_code + '-')
+                    #删除图片
+                    if image_ids:
+                        await self.delete_image(goods_code, image_ids)
+                    # 标记删除成功
+                    await self.mark_as_done(goods_code)
+                except Exception as why:
+                    self.logger.error(f'error while delete image of goodsCode "{goods_code}" cause of {why}')
+        except Exception as why:
+            self.logger.error(f'error while delete image of goodsCode "{goods_code}" cause of {why}')
 
+    async def start(self, sema):
+        tasks = self.pull_tasks()
+        jobs = []
+        await self.login()
+        for row in tasks:
+            jobs.append(asyncio.ensure_future(self.delete_trans(row['goodsCode'], sema)))
+        await asyncio.wait(jobs)
+        await self.session.close()
 
-    async def save(self, goodsCode):
-        sql = "insert into dbo.tpk_goodsCode_del(goodsCode, updateDate) values (%s,%s)"
-        updateDate = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
-        self.cur.execute(sql,(goodsCode, updateDate))
-        self.con.commit()
+    def run(self):
+        loop = asyncio.get_event_loop()
+        sema = asyncio.Semaphore(20)
+        try:
+            loop.run_until_complete(self.start(sema))
+        except Exception as why:
+            self.logger.error(f'fail to delete image cause of {why}')
+        finally:
+            loop.close()
+            self.mongo.close()
+
+    async def mark_as_done(self, goodsCode):
+        self.col.find_one_and_update({'_id': goodsCode},
+                                     {'$set': {f'tupianku{self.tupianku_name}': 1,f'tupianku{self.tupianku_name}UpdatedTime':datetime.datetime.now()}})
+        self.logger.info(f'mark {goodsCode}')
 
 
 if __name__ == '__main__':
     import time
     start = time.time()
-    worker = Worker()
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(worker.run())
+    worker = Worker(1)
+    worker.run()
     end = time.time()
     date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(end))
     print(date + f' it takes {end - start} seconds')
-
