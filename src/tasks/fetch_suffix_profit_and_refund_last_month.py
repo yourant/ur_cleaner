1ed5a223452ace68186f773789915562b8c62a71
tupu <p812816627@163.com>
2020-05-14 17:03:08 +0800

    get last month profit and refund

---
+++ src/tasks/fetch_suffix_profit_and_refund_last_month.py
@@ -0,0 +1,33 @@
+#! usr/bin/env/python3
+# coding:utf-8
+# @Time: 2020-05-14 13:04
+# Author: turpure
+
+import datetime
+from src.tasks.fetch_suffix_profit import ProfitFetcher
+from src.tasks.fetch_suffix_refund import RefundFetcher
+
+
+def get_last_month_first_day():
+    td = datetime.date.today()
+    year = td.year
+    month = td.month
+    if month == 1:
+        year -= 1
+        month = 12
+    else:
+        month -= 1
+    return datetime.date(year, month, 1)
+
+
+if __name__ == "__main__":
+    last_month_first_day = str(get_last_month_first_day())[:10]
+    today = str(datetime.date.today())[:10]
+
+    # fetch suffix profit
+    profit_worker = ProfitFetcher()
+    profit_worker.work(last_month_first_day, today)
+
+    # fetch suffix refund
+    refund_worker = RefundFetcher()
+    refund_worker.work(last_month_first_day, today)
