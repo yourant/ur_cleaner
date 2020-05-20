93c77fdf13c168f3e9b2319831ee87205bede978
liuyang <liuyang@git.com>
2020-05-20 11:45:38 +0800

    create task template

---
+++ src/tasks/task_template.py
@@ -0,0 +1,33 @@
+#! usr/bin/env/python3
+# coding:utf-8
+# @Time: 2019-02-22 11:30
+# Author: turpure
+
+from src.services.base_service import BaseService
+
+
+class Worker(BaseService):
+    """
+    worker template
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def do_something(self):
+        pass
+
+    def work(self):
+        try:
+            self.do_something()
+        except Exception as why:
+            self.logger.error('fail to count sku cause of {} '.format(why))
+        finally:
+            self.close()
+
+
+if __name__ == "__main__":
+    worker = Worker()
+    worker.work()
+
+
