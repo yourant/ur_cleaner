7bac9952f6b5867d874831be983aea06fb0f89c4
shiwei <379171631@qq.com>
2020-05-27 13:35:11 +0800

    wish produnct

--- src/tasks/wish_products_list.py
+++ src/tasks/wish_products_list.py
@@ -13,8 +13,8 @@ from multiprocessing.pool import ThreadPool as Pool
 from pymongo import MongoClient
 
 mongo = MongoClient('192.168.0.150', 27017)
-mongodb = mongo['joom']
-col = mongodb['joom_productlist']
+mongodb = mongo['wish']
+col = mongodb['wish_productlist']
 
 
 class Worker(BaseService):
@@ -25,8 +25,12 @@ class Worker(BaseService):
     def __init__(self):
         super().__init__()
 
-    def get_joom_token(self):
-        sql = 'select AccessToken, aliasName from S_JoomSyncInfo'
+    def get_wish_token(self):
+        sql = ("SELECT AccessToken,aliasname FROM S_WishSyncInfo WHERE  "
+               "aliasname is not null"
+               " and  AliasName not in "
+               "(select DictionaryName from B_Dictionary where CategoryID=12 and used=1 and FitCode='Wish') "
+               )
         self.cur.execute(sql)
         ret = self.cur.fetchall()
         for row in ret:
@@ -34,38 +38,37 @@ class Worker(BaseService):
 
     def clean(self):
         col.delete_many({})
-        self.logger.info('success to clear joom product list')
+        self.logger.info('success to clear wish product list')
 
     def get_products(self, row):
         token = row['AccessToken']
-        suffix = row['aliasName']
-        url = 'https://api-merchant.joom.com/api/v2/product/multi-get'
-        headers = {'content-type': 'application/json', 'Authorization': 'Bearer ' + token}
+        suffix = row['aliasname']
+        url = 'https://merchant.wish.com/api/v2/product/multi-get'
+        # headers = {'content-type': 'application/json', 'Authorization': 'Bearer ' + token}
         date = str(datetime.datetime.today() - datetime.timedelta(days=0))[:10]
-        since = str(datetime.datetime.today() - datetime.timedelta(days=3))[:10]
-        limit = 300
+        # since = str(datetime.datetime.today() - datetime.timedelta(days=3))[:10]
+        limit = 250
         start = 0
         try:
             while True:
                 param = {
                     "limit": limit,
                     'start': start,
-                    'since': since,
+                    'access_token':token
                 }
-                response = requests.get(url, params=param, headers=headers)
+                response = requests.get(url, params=param)
                 ret = response.json()
                 if ret['code'] == 0 and ret['data']:
                     list = ret['data']
                     for item in list:
                         list_variants = item['Product']['variants']
-                        list_enabled = item['Product']['enabled']
-                        list_state = item['Product']['state']
+                        list_removed_by_merchant = item['Product']['removed_by_merchant']
                         for row in list_variants:
                             new_sku = row['Variant']['sku'].split("@")[0]
                             ele = {'code': row['Variant']['sku'], 'sku': row['Variant']['sku'],
                                    'newsku': new_sku, 'itemid': row['Variant']['product_id'], 'suffix': suffix,
                                    'selleruserid': '', 'storage': row['Variant']['inventory'], 'updateTime': date,
-                                   'enabled': list_enabled, 'state': list_state}
+                                   'enabled': row['Variant']['enabled'], 'removed_by_merchant': list_removed_by_merchant }
                             self.put(ele)
                             # self.logger.info(f'putting {row["Variant"]["product_id"]}')
                     start += limit
@@ -80,8 +83,7 @@ class Worker(BaseService):
         col.save(row)
 
     def pull(self):
-        rows = col.find({"enabled": "True",
-                "state": "active"})
+        rows = col.find()
         for row in rows:
             yield (row['code'], row['sku'], row['newsku'], row['itemid'], row['suffix'], row['selleruserid'], row['storage'], row['updateTime'])
 
@@ -113,21 +115,22 @@ class Worker(BaseService):
                 try:
                     self.cur.execute(sql)
                     self.con.commit()
-                    self.logger.info(f"success to save data of joom products from {i * step} to  {min((i + 1) * step, number)}")
+                    self.logger.info(f"success to save data of wish products from {i * step} to  {min((i + 1) * step, number)}")
                 except Exception as why:
-                    self.logger.error(f"fail to save data of joom products cause of {why} ")
+                    self.logger.error(f"fail to save data of wish products cause of {why} ")
         except Exception as why:
-            self.logger.error(f"fail to save joom products cause of {why} ")
+            self.logger.error(f"fail to save wish products cause of {why} ")
 
     def work(self):
         try:
-            tokens = self.get_joom_token()
+            # self.get_wish_token()
+            tokens = self.get_wish_token()
             self.clean()
             pl = Pool(16)
             pl.map(self.get_products, tokens)
             pl.close()
             pl.join()
-            self.save_trans()
+            # self.save_trans()
         except Exception as why:
             self.logger.error('fail to count sku cause of {} '.format(why))
         finally:
