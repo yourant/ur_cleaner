7311c089c04582e3d149cb08a3e26cf058ff1e92
tupu <p812816627@163.com>
2020-05-30 14:59:39 +0800

    wish refund

--- src/tasks/wish_refunded.py
+++ src/tasks/wish_refunded.py
@@ -8,8 +8,13 @@ import datetime
 import requests
 import concurrent
 from concurrent.futures import ThreadPoolExecutor
-from tenacity import retry, stop_after_attempt
 from src.services.base_service import BaseService
+from pymongo import MongoClient
+
+
+mongo = MongoClient('192.168.0.150', 27017)
+mongodb = mongo['wish']
+col = mongodb['wish_refunded']
 
 
 class WishRefund(BaseService):
@@ -35,84 +40,101 @@ class WishRefund(BaseService):
         tokens = self.run_sql(sql)
         return tokens
 
-    @retry(stop=stop_after_attempt(3))
     def get_wish_orders(self, token):
         date = str(datetime.datetime.now() - datetime.timedelta(days=4))[:10]
         url = "https://china-merchant.wish.com/api/v2/order/multi-get"
         start = 0
+        limit = 100
         try:
             while True:
                 data = {
                     "access_token": token['AccessToken'],
                     "format": "json",
                     "start": start,
-                    "limit": 100,
+                    "limit": limit,
                     "since": date,
-                    # "upto": '2018-10-01'
+                    # "since": '2020-05-02',
+                    # "upto": '2020-05-03'
                 }
-                try:
-                    r = requests.get(url, params=data, timeout=10)
-                    res_dict = json.loads(r.content)
+                res_dict = dict()
+
+                for i in range(3):
+                    try:
+                        r = requests.get(url, params=data, timeout=10)
+                        res_dict = json.loads(r.content)
+                        break
+                    except Exception as why:
+                        self.logger.error(f'fail get refund of {token["aliasname"]} {i + 1} times cause of {why}')
+                if res_dict:
                     orders = res_dict['data']
                     for order in orders:
-                        try:
-                            order_detail = order["Order"]
-                            if 'refunds' in order_detail:
-                                all_refunds = order_detail['refunds']
-                                for refunds_info in all_refunds:
-                                    refunds = refunds_info['RefundsInfo']
-                                    refunds['aliasname'] = token['aliasname']
-                                    refunds['order_id'] = order_detail['order_id']
-                                    yield refunds
-                        except Exception as e:
-                            self.logger.debug(e)
-                    order_number = len(orders)
-                    if order_number >= 100:
-                        start += 100
-                    else:
+                        order_detail = order["Order"]
+                        if 'refunds' in order_detail:
+                            all_refunds = order_detail['refunds']
+                            for refunds_info in all_refunds:
+                                refunds = refunds_info['RefundsInfo']
+                                refunds['aliasname'] = token['aliasname']
+                                refunds['order_id'] = order_detail['order_id']
+                                refunds['_id'] = order_detail['transaction_id']
+                                refunds['plat'] = 'wish'
+                                self.put(refunds)
+                    start += limit
+                    if len(orders) < limit:
                         break
-                except Exception as e:
-                    self.logger.debug(e)
-                    break
                 else:
                     break
 
         except Exception as e:
             self.logger.error('{} fails cause of {}'.format(token['aliasname'], e))
 
-    def save_data(self, row):
+    def clean(self):
+        col.delete_many({})
+        self.logger.info('success to clean data')
+
+    def put(self, row):
+        # self.logger.info(f'saving {row["order_id"]}')
+        col.update_one({'_id': row['_id']}, {"$set": row}, upsert=True)
+
+    def pull(self):
+        ret = col.find()
+        for row in ret:
+            yield row
+
+    def push(self, row):
         sql = ("if not EXISTS (select id from y_refunded(nolock) where "
                "order_id=%s and refund_time= %s) "
                "insert into y_refunded (order_id,refund_time, total_value,currencyCode,plat) "
-               "values (%s,%s,%s,%s, 'wish') "
+               "values (%s,%s,%s,%s, %s) "
                "else update y_refunded set "
                "total_value=%s where order_id=%s and refund_time= %s")
         try:
             self.cur.execute(sql,
                              (row['order_id'], row['refund_time'],
                               row['order_id'], row['refund_time'],
-                              row['merchant_responsible_amount'], row['currency_code'],
+                              row['merchant_responsible_amount'], row['currency_code'], row['plat'],
                               row['merchant_responsible_amount'], row['order_id'], row['refund_time']))
             self.con.commit()
             self.logger.info('save %s' % row['order_id'])
         except Exception as e:
             self.logger.error(f'fail to save {row["order_id"]} cause of duplicate key or {e}')
 
+    def save_trans(self):
+        rows = self.pull()
+        for row in rows:
+            self.push(row)
+
     def run(self):
         try:
             tokens = self.get_wish_token()
-            with ThreadPoolExecutor() as pool:
-                ret = {pool.submit(self.get_wish_orders, token): token for token in tokens}
-                for future in concurrent.futures.as_completed(ret):
-                    try:
-                        orders = future.result()
-                        for order in orders:
-                            self.save_data(order)
-                    except Exception as e:
-                        self.logger.error(e)
+            self.clean()
+            with ThreadPoolExecutor(16) as pl:
+                pl.map(self.get_wish_orders, tokens)
+            self.save_trans()
+
         except Exception as e:
             self.logger.error(e)
         finally:
+            mongo.close()
             self.close()
 
 
