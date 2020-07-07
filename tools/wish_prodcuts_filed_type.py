810d13a31b4ca3edc94f31dbb855dde5f2942228
tupu <p812816627@163.com>
2020-07-07 14:06:12 +0800

    wish products datetime formatter

--- tools/wish_prodcuts_filed_type.py
+++ tools/wish_prodcuts_filed_type.py
@@ -4,6 +4,8 @@
 # Author: turpure
 
 import time
+from concurrent.futures import ThreadPoolExecutor as Pool
+from _datetime import datetime
 from pymongo import MongoClient
 
 
@@ -50,11 +52,55 @@ def set_type(row):
         print(f'fail to update {row["_id"]} cause of {why}')
 
 
+def set_date_formatter(row):
+    try:
+        updated_time = row['last_updated']
+        if isinstance(updated_time, int):
+            if len(str(updated_time)) == 10:
+                updated_time = datetime.fromtimestamp(updated_time)
+            if len(str(updated_time)) == 13:
+                updated_time = datetime.fromtimestamp(updated_time / 1000)
+        date_uploaded = row['date_uploaded']
+        if isinstance(date_uploaded, int):
+            if len(str(date_uploaded)) == 10:
+                updated_time = datetime.fromtimestamp(date_uploaded)
+            if len(str(date_uploaded)) == 13:
+                updated_time = datetime.fromtimestamp(date_uploaded / 1000)
+        update_statement = {
+            'last_updated': updated_time, 'date_uploaded': date_uploaded,
+        }
+        col.update_one({'_id': row['_id']}, {
+            '$set': update_statement
+        })
+        # print(f'updating {row["_id"]}')
+    except Exception as why:
+        print(f'fail to update {row["_id"]} cause of {why}')
+
+
 def main():
-    rows = find_all()
-    for ele in rows:
-        set_type(ele)
+    try:
+        rows = find_all()
+        for ele in rows:
+            set_type(ele)
+    except Exception as why:
+        print(f'mission failed cause of {why}')
+    finally:
+        mongo.close()
+
+
+def run():
+    try:
+        rows = find_all()
+        with Pool(32) as pl:
+            pl.map(set_date_formatter, rows)
+    except Exception as why:
+        print(f'mission failed cause of {why}')
+    finally:
+        mongo.close()
 
 
 if __name__ == '__main__':
-    main()
+    begin = time.time()
+    run()
+    end = time.time()
+    print(f' it takes: {end - begin}')
