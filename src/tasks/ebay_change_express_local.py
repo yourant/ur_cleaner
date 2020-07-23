6b8e4660f797cefc6f2577a28287b66a3b55a2c3
tupu <p812816627@163.com>
2020-07-23 14:54:54 +0800

    change ebay express local

--- src/tasks/ebay_change_express_local.py
+++ src/tasks/ebay_change_express_local.py
@@ -71,11 +71,13 @@ class Updater(BaseService):
     def set_order_new_express(self, order):
         # 设置订单新的物流方式,经过派单处理之后，在非E邮宝或者缺货状态中，申请跟踪号
         # logicsWayNID
-        logistics_ways = {'Hermes - UK Standard 48 (Economy 2-3 working days Service)-UKLE': 524,
-                         'Royal Mail - Tracked 48 Parcel':283}
+        logistics_ways = {
+            'Hermes - UK Standard 48 (Economy 2-3 working days Service)-UKLE': 524,
+            'Royal Mail - Tracked 48 Parcel': 283
+        }
 
         sql = f'update p_trade set logicsWayNid = {logistics_ways[order["newName"]]} where nid = {order["nid"]}'
-        # self.cur.execute(sql)
+        self.cur.execute(sql)
         self.logger.info(f'updating {order["nid"]} of {order["suffix"]} set express to {order["newName"]}')
 
     def trans(self, order_time):
@@ -89,7 +91,7 @@ class Updater(BaseService):
             self.trans(order_time)
 
         except Exception as why:
-            print(why)
+            self.logger.error(f'fail to finish task cause of {why}')
         finally:
             pass
 
