9b607a779e32b68442c11ba5b0fbd8d1656401f6
tupu <p812816627@163.com>
2020-08-05 16:23:51 +0800

    queue name

--- test/test_message_consumer.py
+++ test/test_message_consumer.py
@@ -6,11 +6,12 @@
 import pika
 from pika import credentials
 
+queue_name = 'wish-pb'
 credentials = credentials.PlainCredentials('youran', 'youran2020')
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.0.172', port=5672, credentials=credentials))
 channel = connection.channel()
 
-channel.queue_declare(queue='wish')
+channel.queue_declare(queue=queue_name)
 
 
 def callback(ch, method, properties, body):
@@ -18,7 +19,7 @@ def callback(ch, method, properties, body):
 
 
 channel.basic_consume(
-    queue='wish', on_message_callback=callback, auto_ack=True)
+    queue=queue_name, on_message_callback=callback, auto_ack=True)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
