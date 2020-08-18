9226ac1ecafcc1d0bca92a7f553cdc5b90711921
tupu <p812816627@163.com>
2020-08-18 08:47:20 +0800

    unused ebay accounts

--- src/tasks/ebay_fee.py
+++ src/tasks/ebay_fee.py
@@ -43,7 +43,8 @@ class EbayFee(BaseService):
     def get_ebay_token(self):
         sql = ("SELECT notename,max(ebaytoken) AS ebaytoken FROM S_PalSyncInfo"
                " where notename in (select dictionaryName from B_Dictionary "
-               "where  CategoryID=12 and FitCode ='eBay' and used = 0 ) and  notename not in ('01-buy','11-newfashion','eBay-12-middleshine', '10-girlspring')"
+               "where  CategoryID=12 and FitCode ='eBay' and used = 0 ) and "
+               " notename not in ('01-buy','11-newfashion','eBay-12-middleshine', '10-girlspring','eBay-C105-jkl-27','eBay-E48-tys2526','eBay-E50-Haoyiguoji')"
                "  GROUP BY notename")
                # " having notename='eBay-A6-vitalityang1'")
         self.cur.execute(sql)
