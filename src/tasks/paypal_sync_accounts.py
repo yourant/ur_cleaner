79f77efb4dbe8003933b7736427dbb6fc10d315c
tupu <p812816627@163.com>
2020-12-24 10:02:33 +0800

    sync paypal accounts

--- src/tasks/paypal_sync_accounts.py
+++ src/tasks/paypal_sync_accounts.py
@@ -9,7 +9,7 @@ from src.services.base_service import CommonService
 
 class Worker(CommonService):
     """
-    worker template
+    sync payPal account from  puYuan to ur center
     """
 
     def __init__(self):
@@ -25,12 +25,32 @@ class Worker(CommonService):
         self.base_dao.close_cur(self.cur)
         self.base_dao.close_cur(self.warehouse_cur)
 
-    def do_something(self):
-        pass
+    def get_paypal_accounts(self):
+        sql = 'SELECT top 20 e_maill as email, max(syncinfoId) as nid FROM [dbo].[S_PalSyncInfo](nolock) GROUP BY e_maill ORDER BY nid desc '
+        self.cur.execute(sql)
+        ret = self.cur.fetchall()
+        for row in ret:
+            yield row
+
+    def put_paypal_accounts(self, ele):
+        sql = ("insert into y_PayPalStatus (accountName,isUrUsed, isPyUsed,paypalStatus,memo,createdTime,updatedTime) "
+               "values(%s,%s,%s,%s,%s,getdate(),getdate())")
+        try:
+            self.cur.execute(sql, (ele['email'], 0, 1, '使用中', ''))
+            self.logger.info(f'success to put {ele["email"]}')
+
+        except Exception as why:
+            self.logger.error(f'fail to put {ele["email"]} cause of {why}')
+
+    def trans(self):
+        accounts = self.get_paypal_accounts()
+        for ele in accounts:
+            self.put_paypal_accounts(ele)
+        self.con.commit()
 
     def work(self):
         try:
-            self.do_something()
+            self.trans()
         except Exception as why:
             self.logger.error('fail to finish task cause of {} '.format(why))
             name = os.path.basename(__file__).split(".")[0]
